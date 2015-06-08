d402fe86f65ba94dde8dc419f5fda75aa1992ede
Markus Kiss <markus.kiss@netpioneer.de>
2015-06-08 14:40:46 +0200

    Add initial project setup.
     - Update .gitignore.
    
     - Add Spring Starter project stub.

---
+++ src/main/java/de/netpioneer/socialmedia/SocialMediaAnalysisApplication.java
@@ -0,0 +1,12 @@
+package de.netpioneer.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaAnalysisApplication.class, args);
+    }
+}
