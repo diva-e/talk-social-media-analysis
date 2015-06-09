59ad1ae42298da95897012096b86507ccc07f341
Markus Kiss <markus.kiss@netpioneer.de>
2015-06-09 16:15:27 +0200

    Add search input and REST request for sentiment analysis to Angular app.

--- src/main/resources/public/scripts/controllers/SentimentAnalysisController.js
+++ src/main/resources/public/scripts/controllers/SentimentAnalysisController.js
@@ -1,4 +1,16 @@
 'use strict';
 
-angular.module('socialMediaAnalysisApp')
-  .controller('SentimentAnalysisController', function ($scope) {});
+angular.module('socialMediaAnalysisApp').controller(
+		'SentimentAnalysisController', function($scope, $http) {
+
+			$scope.searchKeyword = '';
+
+			$scope.performSentimentAnalysis = function() {
+				console.log('perform analysis with keyword '
+						+ $scope.searchKeyword);
+				$http.get('/social-media-analysis/v1.0/sentiments', { params: { 'keyword': $scope.searchKeyword } })
+				.then(function(result) {
+					$scope.sentimentAnalysisResults = result.data;
+				})};
+
+		});
