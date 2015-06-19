dc7fb6e1afd2fcbba3631ed716450e5641adef85
Markus Kiss <markus.kiss@netpioneer.de>
2015-06-19 13:01:50 +0200

    Add TwitterService for search on Twitter using Spring Social Twitter.

--- src/main/java/de/netpioneer/socialmedia/core/controller/SocialMediaAnalysisController.java
+++ src/main/java/de/netpioneer/socialmedia/core/controller/SocialMediaAnalysisController.java
@@ -1,10 +1,12 @@
 package de.netpioneer.socialmedia.core.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import de.netpioneer.socialmedia.sentimentanalysis.model.Sentiment;
 import de.netpioneer.socialmedia.sentimentanalysis.service.SentimentAnalysisService;
+import de.netpioneer.socialmedia.twitter.service.TwitterService;
 import de.netpioneer.socialmedia.wordprocessing.service.WordFrequencyService;
 
 @RequestMapping(value = "/social-media-analysis/v1.0/", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -23,6 +26,9 @@ public class SocialMediaAnalysisController {
 	
 	@Autowired
 	private WordFrequencyService wordFrequencyService;
+	
+	@Autowired
+	private TwitterService twitterService;
 	
 	@RequestMapping(value = "sentiments", method = RequestMethod.GET)
 	public Map<Sentiment, Double> getSentimentsForTweetsByKeyword(@RequestParam String keyword) {
@@ -37,6 +43,8 @@ public class SocialMediaAnalysisController {
 	@RequestMapping(value = "wordfrequencies", method = RequestMethod.GET)
 	public Map<String, Integer> getWordFrequenciesForTweetsByKeyword(@RequestParam String keyword) {
 		// TODO make call to wordFrequencyService
+		List<Tweet> tweets = twitterService.findTweetsByKeyword(keyword);
+		
 		Map<String, Integer> wordFrequencies = new HashMap<>();
 		wordFrequencies.put("word1", 3);
 		wordFrequencies.put("word2", 6);
---
+++ src/main/java/de/netpioneer/socialmedia/twitter/service/TwitterService.java
@@ -0,0 +1,23 @@
+package de.netpioneer.socialmedia.twitter.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TwitterService {
+	
+	@Autowired
+	private Twitter twitter;
+
+	public List<Tweet> findTweetsByKeyword(String keyword) {
+		SearchResults searchResults = twitter.searchOperations().search(keyword);
+		List<Tweet> tweets = searchResults.getTweets();
+		return tweets;
+	}
+	
+}
