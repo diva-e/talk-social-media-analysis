b0ed3f0b3b60e26bac47341e73955fbd0055013b
Markus Kiss <markus.kiss@netpioneer.de>
2016-02-19 16:50:26 +0100

    Improve search to contain the 100 most recent tweets in English language.

--- src/main/java/de/netpioneer/socialmedia/twitter/service/TwitterService.java
+++ src/main/java/de/netpioneer/socialmedia/twitter/service/TwitterService.java
@@ -3,6 +3,8 @@ package de.netpioneer.socialmedia.twitter.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchParameters;
+import org.springframework.social.twitter.api.SearchParameters.ResultType;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
@@ -15,7 +17,11 @@ public class TwitterService {
 	private Twitter twitter;
 
 	public List<Tweet> findTweetsByKeyword(String keyword) {
-		SearchResults searchResults = twitter.searchOperations().search(keyword, 100);
+		SearchParameters searchParameters = new SearchParameters(keyword)
+				.resultType(ResultType.RECENT)
+				.lang("en")
+				.count(100);
+		SearchResults searchResults = twitter.searchOperations().search(searchParameters);
 		List<Tweet> tweets = searchResults.getTweets();
 		return tweets;
 	}
